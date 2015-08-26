338c0f0fab2f491aff0471617cf499be8776d5b9
David Gil <dgilperez@gmail.com>
2015-08-26 22:20:03 +0200

    adds IceCube.compatibility = 12 to spec_helper to get rid of deprecation warnings from ice_cube

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
 require 'active_support'
 require 'ostruct'
-require 'pry'
+require 'pry-byebug'
 require 'support/parser_macros'
 
-$: << File.expand_path('../lib')
+# $: << File.expand_path('../lib')
 
 RSpec.configure do |config|
   config.include SpecHelpers::ParserMacros
   config.expect_with :rspec
+
+  config.before(:each) do
+    IceCube.compatibility = 12 if defined?(IceCube)
+  end
 end
